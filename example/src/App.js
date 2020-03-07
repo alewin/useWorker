6e52870545fda13585f51925b698095f4f1c6b5d
Alessio Koci <ko.alessio@gmail.com>
2020-03-07 14:27:12 +0100

    feat: improve sorting example

--- example/src/App.js
+++ example/src/App.js
@@ -1,171 +1,53 @@
 import React from "react";
-import useWorker from "@koale/useworker";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Sorting from "./pages/Sorting";
 import logo from "./react.png";
-import "./App.css";
+import "./style.css";
 
-const selectionSort = arr => {
-  const len = arr.length;
-  for (let i = 0; i < len; i++) {
-    let min = i;
-    for (let j = i + 1; j < len; j++) {
-      if (arr[min] > arr[j]) {
-        min = j;
-      }
-    }
-    if (min !== i) {
-      const tmp = arr[i];
-      arr[i] = arr[min];
-      arr[min] = tmp;
-    }
-  }
-  return arr;
-};
+let turn = 0;
+function infiniteLoop() {
+  const lgoo = document.querySelector(".App-logo");
+  turn += 8;
+  lgoo.style.transform = `rotate(${turn % 360}deg)`;
+}
 
-const bubleSort = array => {
-  let swapp;
-  let n = array.length - 1;
-  const sortedArray = array.slice();
-  do {
-    swapp = false;
-    for (let index = 0; index < n; index++) {
-      if (sortedArray[index] > sortedArray[index + 1]) {
-        const tmp = sortedArray[index];
-        sortedArray[index] = sortedArray[index + 1];
-        sortedArray[index + 1] = tmp;
-        swapp = true;
-      }
-    }
-    n--;
-  } while (swapp);
-
-  return sortedArray;
-};
-
-const numbers = [...Array(50000)].map(e => ~~(Math.random() * 1000000));
-
-const initialStates = {
-  bubleSort: false,
-  bubleSortWorker: false,
-  selectionSortWorker: false
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "SET_BUBLE_SORT_LOADING":
-      return {
-        ...state,
-        bubleSort: action.loading
-      };
-    case "SET_BUBLE_SORT_WORKER_LOADING":
-      return {
-        ...state,
-        bubleSortWorker: action.loading
-      };
-    case "SET_SELECTION_SORT_WORKER_LOADING":
-      return {
-        ...state,
-        selectionSortWorker: action.loading
-      };
-    default:
-      return state;
-  }
-};
-
-function App() {
-  const [state, dispatch] = React.useReducer(reducer, initialStates);
-
+export default function App() {
   React.useEffect(() => {
-    let turn = 0;
-    const infiniteLoop = setInterval(function () {
-      const lgoo = document.querySelector(".App-logo");
-      turn += 8;
-      lgoo.style.transform = `rotate(${turn % 360}deg)`;
-    }, 100);
-
-    return () => clearInterval(infiniteLoop)
+    const loopInterval = setInterval(infiniteLoop, 100);
+    return () => clearInterval(loopInterval);
   }, []);
 
-  const [bubleSortWorker, bSortStatus, bSortKill] = useWorker(bubleSort);
-  const [selectionSortWorker, sSortStatus, sSortKill] = useWorker(
-    selectionSort
-  );
-
-  const onSortClick = () => {
-    dispatch({ type: "SET_BUBLE_SORT_LOADING", loading: true });
-    const result = bubleSort(numbers);
-    dispatch({ type: "SET_BUBLE_SORT_LOADING", loading: false });
-    console.log("Buble Sort", result);
-  };
-
-  const onBubleSortWorkerClick = () => {
-    dispatch({ type: "SET_BUBLE_SORT_WORKER_LOADING", loading: true });
-    bubleSortWorker(numbers).then(result => {
-      dispatch({ type: "SET_BUBLE_SORT_WORKER_LOADING", loading: false });
-      console.log("Buble Sort useWorker()", result);
-    });
-  };
-
-  const onSectionSortWorkerOldClick = () => {
-    dispatch({ type: "SET_SELECTION_SORT_WORKER_LOADING", loading: true });
-    selectionSortWorker(numbers).then(result => {
-      dispatch({ type: "SET_SELECTION_SORT_WORKER_LOADING", loading: false });
-      console.log("Selection Sort useWorker()", result);
-    });
-  };
-
-  const killAllWorkers = () => {
-    sSortKill();
-    dispatch({ type: "SET_SELECTION_SORT_WORKER_LOADING", loading: false });
-    bSortKill();
-    dispatch({ type: "SET_BUBLE_SORT_WORKER_LOADING", loading: false });
-  };
   return (
-    <>
+    <Router>
       <div className="App">
         <h1 className="App-Title">useWorker</h1>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <button
-            type="button"
-            disabled={state.bubleSort}
-            className="App-button"
-            onClick={onSortClick}
-          >
-            {state.bubleSort ? `Loading...` : `Buble Sort`}
-          </button>
-          <button
-            type="button"
-            disabled={state.bubleSortWorker}
-            className="App-button"
-            onClick={onBubleSortWorkerClick}
-          >
-            {state.bubleSortWorker ? `Loading...` : `Buble Sort useWorker()`}
-          </button>
-          <button
-            type="button"
-            disabled={state.selectionSortWorker}
-            className="App-button"
-            onClick={onSectionSortWorkerOldClick}
-          >
-            {state.selectionSortWorker
-              ? `Loading...`
-              : `Selection Sort useWorker()`}
-          </button>
-          <button
-            type="button"
-            disabled={!(state.selectionSortWorker || state.bubleSortWorker)}
-            className="App-button"
-            onClick={killAllWorkers}
-          >
-            Kill All Workers
-          </button>
+          <ul>
+            <li>
+              <Link style={{ color: "white" }} to="/sorting">
+                Sorting Demo
+              </Link>
+            </li>
+            <li>
+              <Link style={{ color: "white" }} to="/csv">
+                Csv Demo
+              </Link>
+            </li>
+          </ul>
         </header>
-        <span style={{ color: "white" }}>
-          Open DevTools console to see the results.
-        </span>
+        <hr />
       </div>
-    </>
+      <div>
+        <Switch>
+          <Route path="/sorting">
+            <Sorting />
+          </Route>
+          <Route path="/csv">
+            <Sorting />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ example/src/pages/Sorting/index.js
@@ -0,0 +1,73 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
+import React from "react";
+import useWorker, { WORKER_STATUS } from "../../useWorker";
+
+import bubleSort from "./algorithms/bublesort";
+
+const numbers = [...Array(50000)].map(() =>
+  Math.floor(Math.random() * 1000000)
+);
+
+function App() {
+  const [sortStatus, setSortStatus] = React.useState(false);
+  const [sortWorker, sortWorkerStatus, killWorker] = useWorker(bubleSort);
+
+  console.log("WORKER:", sortWorkerStatus);
+
+  const onSortClick = () => {
+    setSortStatus(true);
+    const result = bubleSort(numbers);
+    setSortStatus(false);
+    alert("Finished: normal Sort");
+    console.log("Buble Sort", result);
+  };
+
+  const onWorkerSortClick = () => {
+    sortWorker(numbers).then(result => {
+      console.log("Buble Sort useWorker()", result);
+      alert("Finished: Sort using useWorker.");
+    });
+  };
+
+  return (
+    <div>
+      <section className="App-section">
+        <button
+          type="button"
+          disabled={sortStatus}
+          className="App-button"
+          onClick={() => onSortClick()}
+        >
+          {sortStatus ? `Loading...` : `Buble Sort`}
+        </button>
+        <button
+          type="button"
+          disabled={sortWorkerStatus === WORKER_STATUS.RUNNING}
+          className="App-button"
+          onClick={() => onWorkerSortClick()}
+        >
+          {sortWorkerStatus === WORKER_STATUS.RUNNING
+            ? `Loading...`
+            : `Buble Sort useWorker()`}
+        </button>
+        {sortWorkerStatus === WORKER_STATUS.RUNNING ? (
+          <button
+            type="button"
+            className="App-button"
+            onClick={() => killWorker()}
+          >
+            Kill Worker
+          </button>
+        ) : null}
+      </section>
+      <section className="App-section">
+        <span style={{ color: "white" }}>
+          Open DevTools console to see the results.
+        </span>
+      </section>
+    </div>
+  );
+}
+
+export default App;
