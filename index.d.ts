397bc4c1195f7c9e1e0a004660dd4b3f27084438
Alessio Koci <ko.alessio@gmail.com>
2020-03-17 19:55:37 +0100

    feat: add useWorker typescript support

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,2 @@
-// Type definitions for @koale/useWorker
-// Project: https://github.com/alewin/useWorker
-// TypeScript Version: 2.8
-
 export type HookReturnType = [(...fnArgs: any[]) => (Promise<any>), string, () => void];
-
-export function useWorker(fn: any[]): HookReturnType;
+export function useWorker(fn: any): HookReturnType;
