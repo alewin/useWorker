2e4389d7164a9dbe76242f9c94388740f4cacab6
Ilja Daderko <IljaDaderko@users.noreply.github.com>
2020-03-23 12:20:13 +0200

    Make HookReturnType inherit argument and return types
    This ensures that `workerSumNumbers` keeps type definitions of `sumNumbers` in use case like this:
    
    ```ts
    function sumNumbers(n1: number, n2: number) {
      return n1 + n2;
    }
    
    const [workerSumNumbers] = useWorker(sumNumbers);
    ```
    
    `workerSumNumbers` will now have proper inherited type of `const workerSumNumbers: (n1: number, n2: number) => Promise<number>`

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,5 @@ export type HookOptions = {
   timeout?: number;
   dependencies?: Array<String>;
 }
-export type HookReturnType = [(...fnArgs: any[]) => (Promise<any>), string, () => void];
-export function useWorker(fn: Function, options?: HookOptions): HookReturnType;
+export type HookReturnType<T> = [(...fnArgs: Parameters<T>) => (Promise<ReturnType<T>>), string, () => void];
+export function useWorker<T>(fn: T, options?: HookOptions): HookReturnType<T>;
