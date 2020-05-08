ab869c961eec20d1e0a98346e5e1041544331176
gonza <gonzalo.christobal@gmail.com>
2020-05-08 15:18:29 -0400

    Add minimal test

---
+++ karma.conf.js
@@ -0,0 +1,46 @@
+module.exports = function (config) {
+  config.set({
+    frameworks: ["mocha", "chai"],
+    files: [{ pattern: "test/*.test.js" }],
+    preprocessors: {
+      "test/*.test.js": ["webpack", "sourcemap"], //preprocess with webpack and our sourcemap loader
+    },
+    reporters: ["dots"], //report results in this format
+    webpack: {
+      devtool: "inline-source-map", //just do inline source maps instead of the default
+      module: {
+        rules: [
+          {
+            test: /test\.js$/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  [
+                    "@babel/preset-env",
+                    {
+                      targets: {
+                        esmodules: true,
+                      },
+                    },
+                  ],
+                ],
+                plugins: [
+                  "@babel/plugin-transform-react-jsx",
+                  "@babel/plugin-proposal-optional-chaining",
+                ],
+              },
+            },
+          },
+        ],
+      },
+    },
+    reporters: ["progress"],
+    port: 9876, // karma web server port
+    colors: true,
+    logLevel: config.LOG_INFO,
+    browsers: ["ChromeHeadless"],
+    autoWatch: false,
+    concurrency: Infinity,
+  });
+};
