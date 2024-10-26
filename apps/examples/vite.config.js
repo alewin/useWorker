648ffc351aa49d8d512b9fb606dcee483ed0142e
Alessio Koci <ko.alessio@gmail.com>
2024-10-26 19:43:19 +0200

    chore: biome ci

--- apps/examples/vite.config.js
+++ apps/examples/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  root: "src",
+  root: 'src',
   build: {
-    outDir: "../dist",
+    outDir: '../dist',
   },
-});
+})
